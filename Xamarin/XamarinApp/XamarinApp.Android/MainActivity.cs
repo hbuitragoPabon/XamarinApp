8e0ba82b78680b067f03b6bae9768df0a0a6d280
Hector Hernan Buitrago Pabon <hector.buitraguito@gmail.com>
2020-05-30 09:37:18 -0500

    configuracion

--- Xamarin/XamarinApp/XamarinApp.Android/MainActivity.cs
+++ Xamarin/XamarinApp/XamarinApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace XamarinApp.Droid
 {
-    [Activity(Label = "XamarinApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "XamarinApp 2020", Icon = "@mipmap/restaurant", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
